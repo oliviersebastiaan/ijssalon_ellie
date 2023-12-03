6da52900613987b0a20e3d10bab9cce4b6376350
Olivier <hello@oliviersebastiaan.com>
2023-12-03 18:30:32 +0200

    oefenopdracht uitgevoerd

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,9 +1,11 @@
-#Les 8 Vraag 1:
+#Les 8 Vraag 2:
 def mijn_functie_1(a):
     return a * a
 
-print(mijn_functie_1(10))
+print(mijn_functie_1(5))
 
-#Les 8 Vraag 2:
-def mijn_functie_2(b):
-    return b #en dan de rekenkundige formule
+#Les 8 Vraag 3:
+def mijn_functie_2(a, b):
+    return a+b, a-b, a*b, a/b
+
+print(mijn_functie_2(10,5))
