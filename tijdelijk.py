bc7a5f17367f1de7c8f9f0b7b5d443474bd9f254
Olivier <hello@oliviersebastiaan.com>
2023-12-03 15:34:27 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,47 +1,26 @@
-print()
+from helper import decoreer
 
-#Vraag 1.3
-prijzen = {
-    "aardbei":int("3"),
-    "vanille":int("4"),
-    "chocolade":int("5")
-}
-aanbieding = prijzen["aardbei"]*0.8
-print("vraag 1.3: ", aanbieding)
+def print_aanbieding():
+    prijzen = {
+        "aardbei":int("3"),
+        "vanille":int("4"),
+        "chocolade":int("5")
+    }
+    aanbieding = prijzen["aardbei"]*0.8
 
-#Vraag 1.4
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
-print("vraag 1.4: ", reclame_tekst)
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
 
-#vraag 1.5
-reclame_tekst2 = reclame_tekst[:62]
-print("vraag 1.5: ", reclame_tekst2)
+    reclame_tekst2 = reclame_tekst[:62]
 
-#vraag 1.6
-reclame_tekst3 = reclame_tekst2.upper()
-print ("vraag 1.6: ", reclame_tekst3)
+    reclame_tekst3 = reclame_tekst2.upper()
 
-#vraag 1.7
-reclame_tekst4 = reclame_tekst3.split(" ")
-print ("vraag 1.7: ", reclame_tekst4)
+    reclame_tekst4 = reclame_tekst3.split(" ")
 
+    for el in reclame_tekst4:
+        if len(el)>4:
+            print(el)
+        else:
+            print(el.lower())
 
-#vraag 1.8
-print("vraag 1.8: ")
-for el in reclame_tekst4:
-    print(el)
-
-#vraag 1.9
-print()
-print("vraag 1.9: ")
-for el in reclame_tekst4:
-    print(el.lower())
-
-#vraag 1.10
-print()
-print("vraag 1.10: ")
-for el in reclame_tekst4:
-    if len(el)>4:
-        print(el)
-    else:
-        print(el.lower())
+decoreer("Aanbieding")
+print_aanbieding()
